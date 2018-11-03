5512d5ffce36457814bf3409cc89ad7879350303
ashimida6 <ashimida6@github.com>
2018-11-03 11:42:29 +0800

    wow!vue & webpack

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
 # RO-Site
 
-Realme.One Foundation website
+> Realme.One Foundation website
 
-## Install
+## We use
+
+| Component | Version | Description |
+|:--------|:----|:----------|
+| yarn | 1.7.0 | component manage |
+| vue-cli | 2.9.6 | init project structure |
+| vue | 2.5.2 | |
+| bootstrap | 4.1.3 | |
+| webpack | 3.6.0 | |
+
+## Build from source
 
 ```shell
 yarn install
+npm run build
+```
+
+## Test
+
+```shell
+npm run dev
+```
+
+## Package
+
+```shell
+npm run build
 ```
---
+++ src/components/CommingSoon.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="text-center d-flex w-100 h-100 p-3 mx-auto flex-column comming-soon">
+		<header class="mb-auto"></header>
+
+		<main role="main" class="">
+			<h1 class="display-2">
+				Coming Soon
+			</h1>
+
+			<p class="lead">
+				Our website is under construction
+			</p>
+		</main>
+
+		<footer class="mt-auto">
+			@ 2018 realme.one
+		</footer>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'CommingSoon'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.comming-soon {
+  color: #fff;
+  text-shadow: 0 0.05rem 0.1rem rgba(0, 0, 0, 0.5);
+  background: #7918f2;
+  background: -webkit-linear-gradient(-45deg, #ac32e4, #7918f2, #4801ff);
+  background: -o-linear-gradient(-45deg, #ac32e4, #7918f2, #4801ff);
+  background: -moz-linear-gradient(-45deg, #ac32e4, #7918f2, #4801ff);
+  background: linear-gradient(-45deg, #ac32e4, #7918f2, #4801ff);
+}
+</style>
